5ea5dd2996c7a3999c4588b7875cc0bdcd5476e1
ChanilPark <49317214+Aeroxol@users.noreply.github.com>
2024-03-12 03:06:03 +0900

    중간저장

--- Assets/Scripts/Ant.cs
+++ Assets/Scripts/Ant.cs
@@ -6,6 +6,12 @@ namespace AnsBuster
 {
     public class Ant : MonoBehaviour
     {
+        public float moveSpeed;
+        public float turnTime;
+
+        private Vector2 m_direction;
+        private float m_turnTimer;
+
         // Start is called before the first frame update
         void Start()
         {
@@ -15,7 +21,20 @@ namespace AnsBuster
         // Update is called once per frame
         void Update()
         {
+            transform.Translate(moveSpeed * m_direction * Time.deltaTime);
+            m_turnTimer -= Time.deltaTime;
+            if(m_turnTimer < 0)
+            {
+                foo();
+                m_turnTimer += turnTime;
+            }
+        }
 
+        private void foo()
+        {
+            float f = 360f * Random.value;
+            Vector3 v = Quaternion.Euler(0, 0, f) * Vector3.right;
+            m_direction = v;
         }
     }
 }
--- Assets/Scripts/GameBoard.cs
+++ Assets/Scripts/GameBoard.cs
@@ -5,20 +5,46 @@ using UnityEngine;
 
 namespace AntBuster
 {
-    public class Node
+    public class Cell
     {
         private int _x, _y;
-        public Node(int x, int y)
+        private bool m_isOccupied;
+        private Cannon m_cannon;
+        public Cell(int x, int y)
         {
             _x = x;
             _y = y;
+        }
+        public void SetCannon(Cannon cannon)
+        {
+            if (cannon == null)
+            {
+                m_isOccupied = false;
+            }
+            else
+            {
+                m_cannon = cannon;
+                m_isOccupied = true;
+            }
+        }
+        public Cannon GetCannon()
+        {
+            if (m_isOccupied)
+            {
+                return m_cannon;
+            }
+            else
+            {
+                return null;
+            }
         }
     }
 
     public class GameBoard : MonoBehaviour
     {
-        private Node[,] m_gameBoard;
+        private Cell[,] m_gameBoard;
         private int m_width, m_height;
+        private List<Cannon> m_cannons = new List<Cannon>();
 
         // Start is called before the first frame update
         void Start()
@@ -34,7 +60,7 @@ namespace AntBuster
 
         public void Generate(int width, int height)
         {
-            m_gameBoard = new Node[width, height];
+            m_gameBoard = new Cell[width, height];
             m_width = width;
             m_height = height;
 
@@ -42,35 +68,45 @@ namespace AntBuster
             {
                 for (int j = 0; j < height; j++)
                 {
-                    m_gameBoard[i, j] = new Node(i, j);
+                    m_gameBoard[i, j] = new Cell(i, j);
                 }
             }
         }
 
-        public List<Node> Evaluate(Vector2Int from, Vector2Int to)
+        public List<Cell> Evaluate(Vector2Int from, Vector2Int to)
         {
             return Evaluate(from.x, from.y, to.x, to.y);
         }
 
-        public List<Node> Evaluate(int fromX, int fromY, int toX, int toY)
+        public List<Cell> Evaluate(int fromX, int fromY, int toX, int toY)
         {
             fromX = Mathf.Clamp(fromX, 0, m_width - 1);
             fromY = Mathf.Clamp(fromY, 0, m_height - 1);
             toX = Mathf.Clamp(toX, 0, m_width - 1);
             toX = Mathf.Clamp(toY, 0, m_height - 1);
 
-            List<Node> route = new List<Node> { m_gameBoard[fromX, fromY] };
-            List<Node> openList = new List<Node>();
-            List<Node> closedList = new List<Node>();
+            List<Cell> route = new List<Cell> { m_gameBoard[fromX, fromY] };
+            List<Cell> openList = new List<Cell>();
+            List<Cell> closedList = new List<Cell>();
 
             while (openList.Count != 0)
             {
-                Node _node = openList[0];
+                Cell _node = openList[0];
                 openList.RemoveAt(0);
                 closedList.Add(_node);
             }
 
             return route;
+        }
+
+        public Cell GetNextCell(Vector2Int v)
+        {
+            return GetNextCell(v.x, v.y);
+        }
+
+        public Cell GetNextCell(int x, int y)
+        {
+            return m_gameBoard[x + 1, y + 1];
         }
 
         private void OnDrawGizmosSelected()
